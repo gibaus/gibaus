344ba8ad30acb84faedb67dc14e74f0345a2e3b9
GiBaus <gibaus@gmail.com>
2024-01-05 23:28:15 +0100

    first commit

---
+++ .history/readme_20240105232516.js
@@ -0,0 +1,37 @@
+module.exports = `### Hi there 👋
+
+I create application on the web and share my journey on [Twitter](https://twitter.com/melvynxdev) and [Youtube](https://www.youtube.com/channel/UC5HDIVwuqoIuKKw-WbQ4CvA) !
+
+And... I create online courses in french to help developers to master modern web technologies.
+
+### Contact me
+
+* [Twitter](https://twitter.com/melvynxdev)
+* [Youtube](https://www.youtube.com/channel/UC5HDIVwuqoIuKKw-WbQ4CvA)
+* [Mail](mailto:contact@melvynx.com)
+* [Linkedin](https://www.linkedin.com/in/melvyn-malherbe/)
+
+### My projects
+
+* [Underconstruction](https://)
+
+My personal website, where you can find my projects, my blog and my resume.
+
+* [Basedeco](https://)
+
+My blog and my newsletter website.
+
+* [My Blog](https://)
+
+### Visitor count
+
+<img src="https://profile-counter.glitch.me/Melvynx/count.svg" />
+
+Added the \`Fri 5 Jan 2024\`
+
+Last update on <#today_date>
+
+<#day_before_new_years> days before new years
+
+<#gabot_signing>
+`;
